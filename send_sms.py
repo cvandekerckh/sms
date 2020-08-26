ea1d222b74a7f71fb029d6d2f90b8f5f3216001e
Corentin Vande Kerckhove <corentin@delphia.com>
2020-08-26 13:43:44 -0400

    incorporate env variables for phones

--- send_sms.py
+++ send_sms.py
@@ -10,19 +10,26 @@ from datetime import datetime
 load_dotenv()
 TWILIO_KEY=os.getenv("TWILIO_KEY")
 TWILIO_TOKEN=os.getenv("TWILIO_TOKEN")
+CORENTIN_PHONE=os.getenv("CORENTIN_PHONE")
+ANNESO_PHONE=os.getenv("ANNESO_PHONE")
+BOT_PHONE=os.getenv("BOT_PHONE")
+
 
 # Authenticate
 client = Client(TWILIO_KEY, TWILIO_TOKEN)
 
 while(True):
     time.sleep(10)
-    if datetime.now() > datetime(2020, 8, 26, 13, 15, 0, 0):
+    if datetime.now() > datetime(2020, 8, 26, 13, 28, 0, 0):
 
 
         # Send message
-        client.messages.create(to="+16472041619",
-                            from_="+16473709956",
-                               body="Bonne course coco :)")
+        client.messages.create(
+            to=CORENTIN_PHONE,
+            from_=BOT_PHONE,
+            body="Bonne course coco :)"
+        )
+
         # Security
         time.sleep(300)
         break
